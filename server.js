0afbea23ab7c50f8d82461d84a239682d7a1a77f
Chaya V K <yadavchaya29@gmail.com>
2026-01-06 12:41:52 +0530

    fgfdgdfg

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-
+const app = express();
 
 app.use(cors(corsOptions));
 const corsOptions = {
@@ -9,7 +9,7 @@ const corsOptions = {
 };
 const bodyParser = require('body-parser');
 
-const app = express();
+
 const PORT = 3000;
 
 // Middleware
